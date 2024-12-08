98695842f9d817609a33b7d0c0b4660200cd23e4
sun02020534 <sun02020534@gmail.com>
2024-12-08 13:34:37 +0000

    Save changes to program.c

--- program.c
+++ program.c
@@ -1,12 +1,21 @@
-<<<<<<< HEAD
-int fib(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    return fib(n-1) + fib(n-2);
+#include <stdio.h>
+
+int fib(int n)
+{
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  return fib(n-1) + fib(n-2);
 }
-=======
-int sum(int a, int b) {
-    return a + b;
+
+int sum(int a, int b)
+{
+  return a + b;
 }
->>>>>>> develop
+
+int main()
+{
+  printf("Hello world!\n");
+  printf("%d\n", fib(5));
+  return 0;
+}
 
