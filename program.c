5f2805876ff1cbc69e2748f1694df4a073ebb160
sun02020534 <sun02020534@gmail.com>
2024-12-08 12:55:49 +0000

    Resolved merge conflict by keeping fib function and removing sum function

--- program.c
+++ program.c
@@ -1,15 +1,12 @@
-#include <stdio.h>
-
-int fib(int n)
-{
-  if (n == 0) return 0;
-  if (n == 1) return 1;
-  return fib(n-1) + fib(n-2);
+<<<<<<< HEAD
+int fib(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fib(n-1) + fib(n-2);
 }
-
-int main()
-{
-  printf("Hello world!\n");
-  printf("%d\n", fib(5));
-  return 0;
+=======
+int sum(int a, int b) {
+    return a + b;
 }
+>>>>>>> develop
+
